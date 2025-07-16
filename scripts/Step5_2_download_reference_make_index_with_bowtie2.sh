3d100f4af6110c6e4006de4226496254060c727d
myunggeunO <160444284+myunggeunO@users.noreply.github.com>
2025-07-16 11:54:42 +0900

    Create Step5_2_download_reference_make_index_with_bowtie2.sh

---
+++ scripts/Step5_2_download_reference_make_index_with_bowtie2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Number of threads to use
+THREADS=7
+
+# 1. Create reference index directory
+mkdir -p reference_index
+cd reference_index
+
+# 2. Download mm10 FASTA file from UCSC
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.fa.gz
+
+# 3. Decompress the FASTA file using pigz
+pigz -p "$THREADS" -d mm10.fa.gz
+
+# 4. Build Bowtie2 index from FASTA
+bowtie2-build mm10.fa mm10
+
+# 5. Print completion message
+echo "Bowtie2 index built from UCSC mm10.fa in $(pwd)"
