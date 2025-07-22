edb3635067e7ceed67c5fb046c56685855815164
myunggeunO <160444284+myunggeunO@users.noreply.github.com>
2025-07-22 21:20:37 +0900

    Update Step8_1_dupulicate_removal_sorting-ChIP_GRO.sh

--- scripts/Step8_1_dupulicate_removal_sorting-ChIP_GRO.sh
+++ scripts/Step8_1_dupulicate_removal_sorting-ChIP_GRO.sh
@@ -2,7 +2,7 @@
 set -e
 THREADS=25
 
-# 1. Removing dupulicate and sorting for H3K27ac and corresponding inputs (for merged technical replicates)
+# 1. Remove dupulicate and sorting for H3K27ac and corresponding inputs (for merged technical replicates)
 echo "Remove dupulicate & sort for H3K27ac and inputs"
 find MATERIAL/H3K27ac -path "*/tmerge/02.Align/*merge_filtered.bam" | while read bam; do
     base="${bam%.bam}"
@@ -11,7 +11,7 @@ find MATERIAL/H3K27ac -path "*/tmerge/02.Align/*merge_filtered.bam" | while read
     rm -f "${base}_rmdup.bam" "${base}_rmdup.bam.bai"
 done
 
-# 2. Removing dupulicate and sorting for H3K4me1 and corresponding inputs
+# 2. Remove dupulicate and sorting for H3K4me1 and corresponding inputs
 echo "Remove dupulicate & sort for H3K4me1 and inputs"
 find MATERIAL/H3K4me1 -type f -name "*_filtered.bam" | while read bam; do
     base="${bam%.bam}"
@@ -21,7 +21,7 @@ find MATERIAL/H3K4me1 -type f -name "*_filtered.bam" | while read bam; do
 done
 
 # 3. Sort only for GRO files (no markdup)
-echo "Sorting GRO-seq reads"
+echo "Sort GRO-seq reads"
 find MATERIAL/GRO -type f -name "*_filtered.bam" | while read bam; do
     base="${bam%.bam}"
     sambamba sort -t $THREADS -o "${base}.sorted.bam" "$bam"
