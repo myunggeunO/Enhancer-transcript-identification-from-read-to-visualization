e944c083e778b7338f4ddfcf9a2d4a59c539cbce
myunggeunO <160444284+myunggeunO@users.noreply.github.com>
2025-07-23 14:30:09 +0900

    Update Step22_genetarion_of_aggregation_plot.sh

--- scripts/Step22_genetarion_of_aggregation_plot.sh
+++ scripts/Step22_genetarion_of_aggregation_plot.sh
@@ -18,28 +18,13 @@ for subset in ES_active_E_intergenic ES_E_intergenic ES_non_active_E_intergenic 
     fi
     matrix_out="$base_dir/03.Matrix/${subset}_aggregation_matrix.gz"
     profile_out="$base_dir/04_1.aggregation/${subset}_aggregation.pdf"
-
+    
     # Plot generagion
-    computeMatrix reference-point \
-        --referencePoint center \
-        -b 5000 -a 5000 \
-        -R "$summit_bed" \
-        -S "$bw_dir/ATAC.bw" "$bw_dir/H3K27ac.bw" "$bw_dir/H3K4me1.bw" \
-        --missingDataAsZero \
-        -p "$THREADS" \
-        -o "$matrix_out"
+    computeMatrix reference-point --referencePoint center -a 5000 -b 5000 -R "$summit_bed" \
+    -S "$bw_dir/ATAC.bw" "$bw_dir/H3K27ac.bw" "$bw_dir/H3K4me1.bw" --missingDataAsZero -p "$THREADS" -o "$matrix_out"
 
-    plotProfile \
-        -m "$matrix_out" \
-        -out "$profile_out" \
-        --plotHeight 8 \
-        --plotWidth 8 \
-        --yMax 6 \
-        --perGroup \
-        --refPointLabel "summit" \
-        --regionsLabel "${subset}" \
-        --samplesLabel ATAC H3K27ac H3K4me1 \
-        --colors '#2ca339' '#e7211a' '#f4da26'
+    plotProfile -m "$matrix_out" -out "$profile_out" --plotHeight 8 --plotWidth 8 --yMax 6 \
+    --perGroup --refPointLabel "summit" --regionsLabel "${subset}" --samplesLabel ATAC H3K27ac H3K4me1 --colors '#2ca339' '#e7211a' '#f4da26'
 
 done
 
