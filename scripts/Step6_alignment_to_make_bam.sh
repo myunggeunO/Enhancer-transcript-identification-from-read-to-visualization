ca9bba2f76c58afd1d3e326e82cb4ec4cbb11f25
myunggeunO <160444284+myunggeunO@users.noreply.github.com>
2025-07-24 11:29:01 +0900

    Update Step6_alignment_to_make_bam.sh

--- scripts/Step6_alignment_to_make_bam.sh
+++ scripts/Step6_alignment_to_make_bam.sh
@@ -9,7 +9,7 @@ find MATERIAL/H3K27ac -type d -name "01.Clean" | while read clean_dir; do
     align_dir="$(dirname "$clean_dir")/02.Align"
     mkdir -p "$align_dir"
 
-    echo "Processing $clean_dir"
+    echo "Process $clean_dir"
     for val1 in "$clean_dir"/*_1_val_1.fq.gz; do
         base=$(basename "$val1" _1_val_1.fq.gz)
         val2="${clean_dir}/${base}_2_val_2.fq.gz"
@@ -20,7 +20,7 @@ find MATERIAL/H3K27ac -type d -name "01.Clean" | while read clean_dir; do
                 -1 "$val1" -2 "$val2" -S "$align_dir/${base}.sam"
 
             if [[ -f "$align_dir/${base}.sam" ]]; then
-                samtools view -b -q 10 "$align_dir/${base}.sam" > "$align_dir/${                                                                                                                                     base}_filtered.bam"
+                samtools view -b -q 10 "$align_dir/${base}.sam" > "$align_dir/${base}_filtered.bam"
                 rm "$align_dir/${base}.sam"
             else
                 echo "[ERROR] SAM not created: $base"
@@ -35,15 +35,15 @@ find MATERIAL/H3K4me1 -type d -name "01.Clean" | while read clean_dir; do
     align_dir="$(dirname "$clean_dir")/02.Align"
     mkdir -p "$align_dir"
 
-    echo "Processing $clean_dir"
+    echo "Process $clean_dir"
     for fq in "$clean_dir"/*.fq.gz; do
         base=$(basename "$fq" _trimmed.fq.gz)
 
         echo "Aligning: $base"
-        bowtie2 -p "$THREADS" -x "$ref_index" -U "$fq" -S "$align_dir/${base}.sa                                                                                                                                     m"
+        bowtie2 -p "$THREADS" -x "$ref_index" -U "$fq" -S "$align_dir/${base}.sam"
 
         if [[ -f "$align_dir/${base}.sam" ]]; then
-            samtools view -b -q 10 "$align_dir/${base}.sam" > "$align_dir/${base                                                                                                                                     }_filtered.bam"
+            samtools view -b -q 10 "$align_dir/${base}.sam" > "$align_dir/${base}_filtered.bam"
             rm "$align_dir/${base}.sam"
         else
             echo "[ERROR] SAM not created: $base"
@@ -57,7 +57,7 @@ find MATERIAL/ATAC -type d -name "01.Clean" | while read clean_dir; do
     align_dir="$(dirname "$clean_dir")/02.Align"
     mkdir -p "$align_dir"
 
-    echo "Processing $clean_dir"
+    echo "Process $clean_dir"
     for val1 in "$clean_dir"/*_1_val_1.fq.gz; do
         base=$(basename "$val1" _1_val_1.fq.gz)
         val2="${clean_dir}/${base}_2_val_2.fq.gz"
@@ -68,7 +68,7 @@ find MATERIAL/ATAC -type d -name "01.Clean" | while read clean_dir; do
                 -1 "$val1" -2 "$val2" -S "$align_dir/${base}.sam"
 
             if [[ -f "$align_dir/${base}.sam" ]]; then
-                samtools view -b -q 30 "$align_dir/${base}.sam" > "$align_dir/${                                                                                                                                     base}_filtered.bam"
+                samtools view -b -q 30 "$align_dir/${base}.sam" > "$align_dir/${base}_filtered.bam"
                 rm "$align_dir/${base}.sam"
             else
                 echo "[ERROR] SAM not created: $base"
@@ -83,15 +83,15 @@ find MATERIAL/GRO -type d -name "01.Clean" | while read clean_dir; do
     align_dir="$(dirname "$clean_dir")/02.Align"
     mkdir -p "$align_dir"
 
-    echo "Processing $clean_dir"
+    echo "Process $clean_dir"
     for fq in "$clean_dir/"*_trimmed_polyA.fq.gz; do
         base=$(basename "$fq" _trimmed_polyA.fq.gz)
 
         echo "Aligning: $base"
-        bowtie2 -p "$THREADS" --very-sensitive -x "$ref_index" -U "$fq" -S "$ali                                                                                                                                     gn_dir/${base}.sam"
+        bowtie2 -p "$THREADS" --very-sensitive -x "$ref_index" -U "$fq" -S "$align_dir/${base}.sam"
 
         if [[ -f "$align_dir/${base}.sam" ]]; then
-            samtools view -b -q 30 "$align_dir/${base}.sam" > "$align_dir/${base                                                                                                                                     }_filtered.bam"
+            samtools view -b -q 30 "$align_dir/${base}.sam" > "$align_dir/${base}_filtered.bam"
             rm "$align_dir/${base}.sam"
         else
             echo "[ERROR] SAM not created: $base"
