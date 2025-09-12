22f66054ec90451b8d6a5cceeba2dcd52a9a1d39
myunggeunO <160444284+myunggeunO@users.noreply.github.com>
2025-09-12 15:33:56 +0900

    Update Step2_package_installation.sh

--- scripts/Step2_package_installation.sh
+++ scripts/Step2_package_installation.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# 1. Detect Mamba/Conda
+if command -v mamba >/dev/null 2>&1; then
+  CREATE_CMD="mamba"
+elif command -v conda >/dev/null 2>&1; then
+  CREATE_CMD="conda"
+else
+  echo "ERROR: Neither 'mamba' nor 'conda' found in PATH. Follow Step1."
+  exit 1
+fi
+
 # List of bioinformatics tools to be installed
 # - sra_tools
 # - trim-galore
@@ -22,24 +32,24 @@ set -e
 # Other:
 # - HOMER
 
-# 1. Install bioinformatics tools
-mamba install -c bioconda -c conda-forge sra-tools && echo "sra-tools (fasterq-dump, prefetch) installed"
-mamba install -c bioconda -c conda-forge trim-galore && echo "trim-galore installed"
-mamba install -c conda-forge pigz && echo "pigz installed"
-mamba install -c bioconda -c conda-forge bowtie2 -y && echo "bowtie2 installed"
-mamba install -c bioconda -c conda-forge bedtools -y && echo "bedtools installed"
-mamba install -c bioconda -c conda-forge cutadapt -y && echo "cutadapt installed"
-mamba install -c bioconda -c conda-forge fastqc -y && echo "fastqc installed"
-mamba install -c bioconda -c conda-forge macs3 -y && echo "macs3 installed"
-mamba install -c bioconda -c conda-forge subread -y && echo "featureCounts (subread) installed"
-mamba install -c bioconda -c conda-forge samtools -y && echo "samtools installed"
-mamba install -c bioconda -c conda-forge sambamba -y && echo "sambamba installed"
-mamba install -c bioconda -c conda-forge deeptools -y && echo "deeptools installed"
-mamba install -c bioconda -c conda-forge ucsc-bedgraphtobigwig -y && echo "bigWigbuilder (bedgraphtobigwig) installed"
+# 2. Install bioinformatics tools
+${CREATE_CMD} install -c bioconda -c conda-forge sra-tools && echo "sra-tools (fasterq-dump, prefetch) installed"
+${CREATE_CMD} install -c bioconda -c conda-forge trim-galore && echo "trim-galore installed"
+${CREATE_CMD} install -c conda-forge pigz && echo "pigz installed"
+${CREATE_CMD} install -c bioconda -c conda-forge bowtie2 -y && echo "bowtie2 installed"
+${CREATE_CMD} install -c bioconda -c conda-forge bedtools -y && echo "bedtools installed"
+${CREATE_CMD} install -c bioconda -c conda-forge cutadapt -y && echo "cutadapt installed"
+${CREATE_CMD} install -c bioconda -c conda-forge fastqc -y && echo "fastqc installed"
+${CREATE_CMD} install -c bioconda -c conda-forge macs3 -y && echo "macs3 installed"
+${CREATE_CMD} install -c bioconda -c conda-forge subread -y && echo "featureCounts (subread) installed"
+${CREATE_CMD} install -c bioconda -c conda-forge samtools -y && echo "samtools installed"
+${CREATE_CMD} install -c bioconda -c conda-forge sambamba -y && echo "sambamba installed"
+${CREATE_CMD} install -c bioconda -c conda-forge deeptools -y && echo "deeptools installed"
+${CREATE_CMD} install -c bioconda -c conda-forge ucsc-bedgraphtobigwig -y && echo "bigWigbuilder (bedgraphtobigwig) installed"
 
 # 2. Install R packages
-mamba install -c conda-forge r-tidyverse -y && echo "r-tidyverse installed"
-mamba install -c conda-forge r-cowplot -y && echo "r-cowplot installed"
+${CREATE_CMD} install -c conda-forge r-tidyverse -y && echo "r-tidyverse installed"
+${CREATE_CMD} install -c conda-forge r-cowplot -y && echo "r-cowplot installed"
 
 # 3. Install HOMER
 echo "!Installing HOMER..."
